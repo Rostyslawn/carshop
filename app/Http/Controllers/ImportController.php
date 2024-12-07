e28749205b6eb4ceeb339b407267c8bb831eceed
Rostyslaw <rostyslawn02@gmail.com>
2024-12-07 13:04:17 +0200

    optimization

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\products;
 use Database\Seeders\ProductsSeeder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class ImportController extends Controller
@@ -15,6 +16,8 @@ class ImportController extends Controller
 
         if(!$file) return redirect("/")->with('error', 'Ошибка при обработке файла');
 
+        products::query()->delete();
+
         $spreadsheet = IOFactory::load($file->getPathname());
         $sheet = $spreadsheet->getActiveSheet();
         $rows = $sheet->toArray(null, true, true, true);
@@ -22,11 +25,22 @@ class ImportController extends Controller
         foreach ($rows as $index => $row) {
             if ($index == 1) continue;
 
-            $product = new products();
-            $product->product_id = $row["A"];
-            $product->name = $row["B"];
-            $product->price = (float) $row["C"];
-            $product->save();
+            $products[] = [
+                'product_id' => $row["A"],
+                'name' => $row["B"],
+                'price' => (float) $row["C"],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+
+            if (count($products) >= 1000) {
+                DB::table("products")->insert($products);
+                $products = [];
+            }
+        }
+
+        if (count($products) > 0) {
+            DB::table('products')->insert($products);
         }
 
         return redirect("/")->with('success', 'Данные успешно импортированы!');
